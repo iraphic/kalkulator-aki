f4500abfafc660b8fa0ab9b3e633ad321c7a5398
Muhammad Rafii Naufal <rafii.naufal.213@gmail.com>
2025-09-29 16:24:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 export default defineConfig({
   plugins: [
     react(),
+    base: '/kalkulator-aki/',
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
